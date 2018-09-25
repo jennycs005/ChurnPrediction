132dbab07ccad5c975f6e7752c428e0ff34fab83
Chunjie <jennycs005@gmail.com>
2018-09-25 15:13:58 -0400

    Update UserChurnPrediction.py

--- UserChurnPrediction.py
+++ UserChurnPrediction.py
@@ -1,2 +1,10 @@
 #Part 1: Data Exploration
 #Part 1.1: Understand the Raw Dataset
+import warnings
+warnings.filterwarnings('ignore')
+
+import pandas as pd
+import numpy as np
+pd.set_option('display.max_columns', None)
+
+churn_df = pd.read_csv('churn.all')
